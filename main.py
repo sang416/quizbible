b675e3934b50463f1f440afd41578d1b34dae20c
SANGGYUN LEE <94345744+sang416@users.noreply.github.com>
2024-11-09 16:16:51 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///visitors.db'
